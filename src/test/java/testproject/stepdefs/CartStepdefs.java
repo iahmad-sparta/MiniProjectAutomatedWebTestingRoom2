d1ed5f91c4e924cc820c5efcb2a29de2d5f72e30
Robert Helps <sc21rh@leeds.ac.uk>
2025-01-16 11:31:52 +0000

    adding cart stepdefs

---
+++ src/test/java/testproject/stepdefs/CartStepdefs.java
@@ -0,0 +1,30 @@
+package testproject.stepdefs;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+
+public class CartStepdefs {
+    @And("I click the cart button")
+    public void iClickTheCartButton() {
+    }
+
+    @And("I click the checkout button")
+    public void iClickTheCheckoutButton() {
+    }
+
+    @And("I verify details")
+    public void iVerifyDetails() {
+    }
+
+    @And("I click place order")
+    public void iClickPlaceOrder() {
+    }
+
+    @Then("I enter my payment details")
+    public void iEnterMyPaymentDetails() {
+    }
+
+    @And("I click pay and confirm order")
+    public void iClickPayAndConfirmOrder() {
+    }
+}

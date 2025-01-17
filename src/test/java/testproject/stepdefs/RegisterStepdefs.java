3d0fa74ec9a49fb4c602039679be3728e18794a6
DanielFSparta <dfisaha@spartaglobal.com>
2025-01-17 11:14:30 +0000

    Added register page step defs

--- src/test/java/testproject/stepdefs/RegisterStepdefs.java
+++ src/test/java/testproject/stepdefs/RegisterStepdefs.java
@@ -9,12 +9,15 @@ import io.cucumber.java.en.When;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import testproject.pages.Website;
+import testproject.pages.auth.RegisterPage;
 
 import java.io.IOException;
 
 public class RegisterStepdefs {
     private Website website;
     private static final String BASE_URL = "https://automationexercise.com/";
+
+    RegisterPage registerPage;
 
     @Before
     public void setup() throws IOException {
@@ -56,22 +59,41 @@ public class RegisterStepdefs {
     @Given("I am on the Singup Page")
     public void iAmOnTheSingupPage() {
 //        this.website = TestSetup.getWebsite(BASE_URL + "signup");
+        registerPage = website.getRegisterPage();
     }
 
     @And("I fill in the personal information")
     public void iFillInThePersonalInformation() {
-
+        registerPage.selectTitle("Mr");
+        registerPage.enterName(" tester");
+        registerPage.enterPassword("tester");
+        registerPage.selectDay("1");
+        registerPage.selectMonth("1");
+        registerPage.selectYear("2000");
     }
 
     @And("I fill in the address information")
     public void iFillInTheAddressInformation() {
+        registerPage.enterFirstName("tester_first");
+        registerPage.enterLastName("tester_last");
+        registerPage.enterCompany("tester_company");
+        registerPage.enterAddress1("tester_address1");
+        registerPage.enterAddress2("tester_address2");
+        registerPage.enterCity("tester_city");
+        registerPage.selectCountry("United States");
+        registerPage.enterState("tester_state");
+        registerPage.enterZipcode("12345");
+        registerPage.enterMobileNumber("1234567890");
     }
 
     @When("I click Create Account")
     public void iClickCreateAccount() {
+        registerPage.clickRegister();
     }
 
     @Then("The account has been created")
     public void theAccountHasBeenCreated() {
+        String url = TestSetup.getWebsite().getCurrentUrl();
+        MatcherAssert.assertThat(url, Matchers.containsString("account_created"));
     }
 }
